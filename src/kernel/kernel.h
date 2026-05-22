a7082e7de3db6c43384743fad3d866bc52e8133c
MECFORCH <mecforch@gmail.com>
2026-05-22 16:08:05 +0300

    Add kernel header file with VGA terminal definitions

---
+++ src/kernel/kernel.h
@@ -0,0 +1,44 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+/* VGA Buffer */
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+#define VGA_MEMORY 0xB8000
+
+typedef enum {
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE = 1,
+    VGA_COLOR_GREEN = 2,
+    VGA_COLOR_CYAN = 3,
+    VGA_COLOR_RED = 4,
+    VGA_COLOR_MAGENTA = 5,
+    VGA_COLOR_BROWN = 6,
+    VGA_COLOR_LIGHT_GREY = 7,
+    VGA_COLOR_DARK_GREY = 8,
+    VGA_COLOR_LIGHT_BLUE = 9,
+    VGA_COLOR_LIGHT_GREEN = 10,
+    VGA_COLOR_LIGHT_CYAN = 11,
+    VGA_COLOR_LIGHT_RED = 12,
+    VGA_COLOR_LIGHT_MAGENTA = 13,
+    VGA_COLOR_LIGHT_BROWN = 14,
+    VGA_COLOR_WHITE = 15,
+} vga_color_t;
+
+typedef struct {
+    size_t row;
+    size_t column;
+    uint8_t color;
+} terminal_t;
+
+/* Fonksiyonlar */
+void terminal_initialize(void);
+void terminal_putchar(char c);
+void terminal_write(const char *data, size_t size);
+void terminal_writestring(const char *data);
+void kernel_main(uint32_t magic, uint32_t addr);
+
+#endif
